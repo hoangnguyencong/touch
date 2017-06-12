4cb36f5ffb4b5b330dc92bbc31cd773e79efde6a
Hoang Nguyen <hoang.nguyencong@asnet.com.vn>
2017-06-12 14:51:53 +0700

    a

--- backend/src/test/helpers.js
+++ backend/src/test/helpers.js
@@ -3,7 +3,6 @@ import chai from 'chai'
 import app from '../index.js'
 import models from '../models'
 import config from '../libs/constants'
-var fs = require('fs')
 
 global.config = config
 global.apiPrefix = '/api/v1'
@@ -11,11 +10,3 @@ global.models = models
 global.app = app
 global.request = supertest(app)
 global.expect = chai.expect
-
-process.on('exit', (code) => {
-  console.log(`About to exit with code11: ${code}`);
-  fs.writeFileSync('test/result/test_result', code, function (err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-  })
-});
